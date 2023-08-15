0b1a5d0fa7c19451758f49292613a9f9c0adfde9
mmariotti08 <mariottimatias08@gmail.com>
2023-08-15 19:56:51 -0300

    rutas products terminadas

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -3,10 +3,11 @@ const {getProductsController} = require('../controllers/products/getProductsCont
 const {getIdController} = require('../controllers/products/getIdController.js')
 const {createProductController}=require('../controllers/products/createProductController.js')
 const {updatePrductController}=require('../controllers/products/updatePrductController.js')
-
+const {getDraftController}=require('../controllers/products/getDraftController.js')
 productsRoutes.get('/', getProductsController);
 productsRoutes.get('/:id', getIdController)
 productsRoutes.post('/', createProductController)
 productsRoutes.put('/:id', updatePrductController)
+productsRoutes.get('/draft', getDraftController)
 
 module.exports = productsRoutes;
