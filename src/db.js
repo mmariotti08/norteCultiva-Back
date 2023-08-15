f8d92afda52a87465e0065e67f847ad3f6f5d562
mmariotti08 <mariottimatias08@gmail.com>
2023-08-14 22:53:43 -0300

    model Order

--- src/db.js
+++ src/db.js
@@ -5,20 +5,22 @@ const fs = require('fs');
 require("dotenv").config();
 const productModel = require('./models/Product')
 const usersModel = require('./models/Users')
+const orderModel = require('./models/Order')
 const { DB_USER, DB_PASSWORD, DB_HOST  } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/norteCultiva`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/nortecultiva`, {
   logging: false,
   native: false
 });
 
 productModel(sequelize)
 usersModel(sequelize)
+orderModel(sequelize)
 
 
 
 
-const { Product, User} = sequelize.models;
+const { Product, User, Order,} = sequelize.models;
 
 
 
