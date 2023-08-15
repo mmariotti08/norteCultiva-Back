602b1a1614428e1145a51cef7d8e5ec3779c74a7
JuanCruz <juancruzgenovese22@gmail.com>
2023-08-14 22:54:51 -0300

    Carrito terminado

--- src/models/Carrito.js
+++ src/models/Carrito.js
@@ -3,14 +3,42 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
     sequelize.define("Carrito", {
-        id: {},
-        productId: {},
-        userId: {},
-        name: {},
-        main_picture_url: {},
-        retail_price_cents: {},
-        size: {},
-        quentity: {}
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        productId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "Product",
+                key: "id"
+            }
+        },
+        userId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: "Users",
+                key: "id",
+            }
+        },
+        name: {
+            type: DataTypes.STRING
+        },
+        main_picture_url: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+        },
+        retail_price_cents: {
+            type: DataTypes.INTEGER
+        },
+        size: {
+            type: DataTypes.STRING
+        },
+        quentity: {
+            type: DataTypes.INTEGER
+        }
     },{
         timestamps: true,
     })
