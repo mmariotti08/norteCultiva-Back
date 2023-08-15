4dd17638d811a95e73848e33984a3b4f43e5204b
JuanCruz <juancruzgenovese22@gmail.com>
2023-08-15 17:06:29 -0300

    .

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const { Sequelize } = require('sequelize');
-require("dotenv").config();
-const productModel = require('./models/Product')
-const usersModel = require('./models/Users')
-const orderModel = require('./models/Order')
+const productModel = require('./models/Product');
+const usersModel = require('./models/Users');
+const orderModel = require('./models/Order');
+const stockModel = require("./models/Stock");
 const { DB_USER, DB_PASSWORD, DB_HOST  } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/nortecultiva`, {
@@ -11,18 +11,19 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
   native: false
 });
 
+
 productModel(sequelize)
 usersModel(sequelize)
 orderModel(sequelize)
+stockModel(sequelize)
 
 
-
-
-const { Product, User, Order, Stock} = sequelize.models;
+const { Product, User, Order, Stock } = sequelize.models;
 
 Product.hasMany(Stock, { foreignKey: 'productId' });
 Stock.belongsTo(Product, { foreignKey: 'productId' });
 
+console.log("los modelos de sequelize son : ", sequelize.models)
 
 module.exports = {
     ...sequelize.models, 
